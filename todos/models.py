481eace912a8558d336937dfc1baf1c6e348bc30
Uchenna Adubasim <uchadubs@yahoo.com>
2025-07-22 19:58:42 +0100

    push to GH

---
+++ todos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Todo(models.Model):
+    """Model a task."""
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True, editable=False
+        )
+    short_description = models.CharField(max_length=300)
+    date_created = models.DateTimeField('Date', auto_now_add=True)
+    is_done = models.BooleanField()
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f'{self.title.lower()}'
---
+++ todos/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Todo
+
+
+
+class TodoListView(ListView):
+    """List of todos."""
+    model = Todo
+    template_name = 'todos/todo_list.html'
+    context_object_name = 'todos'
+
+
+class TodoDetailView(DetailView):
+    "A todo."
+    model = Todo
+    template_name = 'todos/todo_detail.html'
+
+
+class TodoCreateView(CreateView):
+    """Create a todo."""
+    model = Todo
+    template_name = 'todos/todo_create.html'
+    fields = '__all__'
+    success_url = "http://127.0.0.1:8000/"
